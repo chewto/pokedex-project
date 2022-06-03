271ee8b7e4aedd7c92450dcf7f1d81a2c677a41e
chewto <jesuselozada@gmail.com>
2022-06-03 18:15:07 -0400

    autocomplete finish

--- src/components/PokedexForm.jsx
+++ src/components/PokedexForm.jsx
@@ -1,32 +1,99 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 import '../styles/PokedexForm.css';
 
 const PokedexForm = ({setPokemonID, setLoading, setError}) => {
-	const [pokemon,setPokemon] = useState('')
+
+	const [options, setOptions] = useState([])
+	const [shouldDisplayOptions, setShouldDisplayOptions] = useState(false)
+	const [searchText,setSearchText] = useState('')
+	const wrapperRef = useRef(null)
+
+	useEffect(()=>{
+		const pokemon = [];
+		const promises = new Array(386)
+		.fill().map((v,i) =>fetch(`https://pokeapi.co/api/v2/pokemon-form/${i + 1}`));
+		Promise.all(promises).then(pokemonArr => {
+			return pokemonArr.map(value => {
+				value.json().then(({name ,sprites :{ front_default: sprite}})=>{
+					pokemon.push({name, sprite})
+				})
+			})
+		});
+		setOptions(pokemon)
+	},[])
+
+	useEffect(()=>{
+		window.addEventListener('mousedown',handleClickOutside)
+		return () =>{
+			window.removeEventListener('mousedown', handleClickOutside)
+		}
+	})
+
+	const handleClickOutside = (e) =>{
+		const wrapper = wrapperRef.current
+		if(wrapper && !wrapper.contains(eventTarget)){
+			setShouldDisplayOptions(false)
+		}
+	}
+
 	const handleSubmit = e =>{
 		e.preventDefault();
-		if(pokemon !== ''){
+		if(searchText !== ''){
 			setError(true)
 			setLoading(true)
-			const pokemonID = window.isNaN(parseInt(pokemon)) ? pokemon.toLowerCase() : pokemon
+			const pokemonID = window.isNaN(parseInt(searchText)) ? searchText.toLowerCase() : searchText
 			setPokemonID(pokemonID)
-			setPokemon('')
+			setSearchText('')
 		}
 		setError(false)
 	}
+
+	const onChange = (e) =>{
+		setSearchText(e.target.value)
+	}
+
+	const handleOptionClick = (poke) =>{
+		setSearchText(poke)
+		setShouldDisplayOptions(false)
+	}
+
 	return(
-		<form className='pokemon-form' onSubmit={handleSubmit}>
-			<input 
-				type="text"
-				placeholder="buscar"
-				name="pokemon"
-				className='pokemon-input'
-				value={pokemon}
-				onChange= {e => setPokemon(e.target.value)}
-				autoComplete='off'
-			/>
-			<input type="submit" className='pokemon-btn' value='search'/>	
-		</form>
+		<div res={wrapperRef}>
+			<form className='pokemon-form' onSubmit={handleSubmit}>
+				<input 
+					type="text"
+					placeholder="buscar"
+					name="pokemon"
+					className='pokemon-input'
+					value={searchText}
+					onChange={onChange}
+					onClick={()=>setShouldDisplayOptions(!shouldDisplayOptions)}
+					autoComplete='off'
+				/>
+				<input type="submit" className='pokemon-btn' value='search'/>	
+			</form>
+				{shouldDisplayOptions && (
+					<div className='pokemon-options'>
+						{options.filter(({name})=>name.indexOf(searchText.toLowerCase())> -1).slice(0,4)
+						.map((value, i)=>{
+							return(
+								<div
+									onClick={()=>handleOptionClick(value.name)}
+									key={i}
+									tabIndex="0"
+									className='option'
+								>
+									<span className='span'>{value.name}</span>
+									<img 
+										src={value.sprite} 
+										alt={value.name}
+										/>
+									</div>
+							);
+						})}
+					</div>	
+				)}
+		</div>	
 	)
 }
 
